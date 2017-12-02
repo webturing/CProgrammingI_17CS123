e2006acc54c8488a6e179df7ca74bc4fa3e99af0
zj <zj@ahstu.cc>
2017-12-02 12:27:38 +0800

    实用的质数判断模块，复杂度O(sqrt(n)/2)

--- lec1314-function/prime.c
+++ lec1314-function/prime.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int prime(int n) {
+/*实用的质数判断模块，复杂度O(sqrt(n)/2)*/
+int isPrimer(int n) {
   if (n == 2) return 1;
   if (n % 2 == 0 || n <= 1) return 0;
   int c;
@@ -9,10 +10,10 @@ int prime(int n) {
   return 1;
 }
 int main() {
-  printf("%d\n", 1 == prime(2));  // expectedValue==actualValue
-  printf("%d\n", 0 == prime(9));
-  printf("%d\n", 0 == prime(1));
-  printf("%d\n", 1 == prime(17));
-  printf("%d\n", 1 == prime(1000000007));
+  printf("%d\n", 1 == isPrimer(2));  // expectedValue==actualValue
+  printf("%d\n", 0 == isPrimer(9));
+  printf("%d\n", 0 == isPrimer(1));
+  printf("%d\n", 1 == isPrimer(17));
+  printf("%d\n", 1 == isPrimer(1000000007));
   return 0;
 }
