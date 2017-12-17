1a7c2f39969390c9e8e6167981252c2f84bb4c38
ZHAO Jing <zj@ahstu.cc>
2017-12-17 14:36:03 +0800

    上传文件至 'lec1920-string'

--- lec1920-string/1573.c
+++ lec1920-string/1573.c
@@ -2,14 +2,22 @@
 #include<string.h>
 int main()
 {
-    char s[100]="ABBAABBA";
-    int len=strlen(s);///#include<string.h>
-    printf("%d\n",len);
-    s[4]=0;
-    puts(s);
-    //strcpy
-    //strcat
-    //strcmp
-    //strstr
+    char s[100]="ABBAAABBA";
+    int n=strlen(s);
+    while(n>=2&&n%2==0)
+    {
+        int find=0,i;
+        for(i=0; i<n/2; i++)
+            if(s[i]!=s[n-1-i])
+            {
+                find=1;
+                break;
+            }
+        if(find==1)
+            break;
+        s[n/2]=0;
+        n/=2;
+    }
+    printf("%d\n",n);
 
 }
---
+++ lec1920-string/decode2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char text[1000]="N qtaj dtz.";
+    int k;
+    for(k=0;k<26;k++){
+        printf("k=%d ",k);
+        char code[1000];
+        memset(code,0,sizeof(code));
+        int n=strlen(text);
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(text[i]<='Z'&&text[i]>='A')
+                code[i]=(text[i]-'A'+k)%26+'A';
+            else if(text[i]<='z'&&text[i]>='a')
+                code[i]=(text[i]-'a'+k)%26+'a';
+            else
+                code[i]=text[i];
+        }
+
+        puts(code);
+    }
+    return 0;
+}
